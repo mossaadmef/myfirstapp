76c72144c9ad4fc5640a2bee9294f7888f248ff8
mossaadmef <mossaadmef@gmail.com>
2023-05-04 07:58:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      <p> werklflf;;v </p>
+      <button classNameName='btn btn-success'> MOssaad </button>
+      <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" classNameName="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+</div>
+<div classNameName="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+    </div>
+  );
+}
+
+export default App;
